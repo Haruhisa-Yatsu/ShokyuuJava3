9a9a29c4cdf76b4a2f4e2013172570ff4b5395e5
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2024-05-14 10:45:09 +0900

    3.1 塗りつぶした四角形を描く

---
+++ 初級Java2.5/src/RectTest.java
@@ -0,0 +1,6 @@
+
+public class RectTest extends MyFrame{
+	public void run() {
+		fillRect(30,50,200,100);
+	}
+}
--- 初級Java2.5/src/Start.java
+++ 初級Java2.5/src/Start.java
@@ -4,17 +4,9 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 	
-		int x = 2;
-		int y = 3;
+		System.out.println("プログラムを実行します");
+		MyFrame frame1 = new RectTest();
 		
-		System.out.println(x * y);
-		
-
-		MyFrame frame3 = new RectFrame();
-		frame3.setLocation(350, 550);
-		MyFrame frame4 = new RectFrame();
-		frame4.setLocation(550, 350);
-
 	}
 
 }
