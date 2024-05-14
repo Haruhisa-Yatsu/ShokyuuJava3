36148b467217a76849241d18edb63a39512b2e08
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2024-05-14 11:48:10 +0900

    3.2 四角形をいくつか描いてみよう

--- 初級Java2.5/src/RectTest.java
+++ 初級Java2.5/src/RectTest.java
@@ -1,6 +1,10 @@
 
 public class RectTest extends MyFrame{
 	public void run() {
-		fillRect(30,50,200,100);
+		fillRect(30,40,50,100);
+		fillRect(130,40,50,100);
+		fillRect(30,200,150,10);
+		
+		fillRect(90,160,30,30);
 	}
 }
