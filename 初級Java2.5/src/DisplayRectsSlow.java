cad7d7293906c9de44cc0e6dbb8f5f63851d4107
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2024-05-21 11:54:26 +0900

    5.1 図形をゆっくり表示する

---
+++ 初級Java2.5/src/DisplayRectsSlow.java
@@ -0,0 +1,19 @@
+
+public class DisplayRectsSlow extends MyFrame{
+	public void run() {
+		
+		setColor(0,128,0);
+		
+		int x = 30;
+		
+		while(x <= 170) {
+			
+			fillRect(x,80,10,100);
+			x += 20;
+			// 処理を指定した秒数停止する
+			sleep(1);
+			
+		}
+	}
+
+}
--- 初級Java2.5/src/Start.java
+++ 初級Java2.5/src/Start.java
@@ -5,7 +5,7 @@ public class Start {
 		// TODO 自動生成されたメソッド・スタブ
 	
 		System.out.println("プログラムを実行します");
-		MyFrame frame1 = new RectIf();
+		MyFrame frame1 = new DisplayRectsSlow();
 		
 	}
 
