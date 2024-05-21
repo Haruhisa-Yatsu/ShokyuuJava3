39be4afdc56b2f86b70a385571a2f8858186b4a9
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2024-05-21 10:47:29 +0900

    おまけ課題その２

--- 初級Java2.5/src/RectIf.java
+++ 初級Java2.5/src/RectIf.java
@@ -2,16 +2,16 @@
 public class RectIf extends MyFrame{
 	public void run() {
 		
-		int x = 0;
+		int x = 30;
 		int h = 20;
 		setColor(0,128,0);
 		
 		int i;
 		for(i=0; i<10;i++) {
 
-			fillRect(x,x,h,h);
-		
-			x+=h;
+			fillRect(x,100,10,150 - h);
+			fillRect(x,300 - h,10,h);
+			x+=20;
 			h+=10;
 		}
 	}
