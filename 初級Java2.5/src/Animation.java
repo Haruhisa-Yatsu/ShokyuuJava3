a9bb0f96dcde11dc7810898e0f66525f99db4335
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2024-05-27 11:41:16 +0900

    おまけ問題1

--- 初級Java2.5/src/Animation.java
+++ 初級Java2.5/src/Animation.java
@@ -4,6 +4,8 @@ public class Animation extends MyFrame {
 	int y = 30;
 	int x = 30;
 
+	int nextY = 0;
+
 	private void MoveRect(int _x, int _y) {
 		//(1)画面を消す
 		clear();
@@ -20,15 +22,27 @@ public class Animation extends MyFrame {
 
 	public void run() {
 
-		for (int i = 0; i < 3; i++) {
+		while (true) {
+			
 			while (x <= 200) {
 
 				MoveRect(5, 0);
+			}
+			
+			nextY = y + 50;
+			while (y <= nextY) {
+				MoveRect(0, 5);
 			}
 
 			while (x >= 30) {
 
 				MoveRect(-5, 0);
+			}
+
+			nextY = y + 50;
+			while (y <= nextY) {
+				
+				MoveRect(0, 5);
 			}
 		}
 	}
