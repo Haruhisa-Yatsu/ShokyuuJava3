31325943ceae5840579a7ce14b0068ed12a4f26b
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2024-05-28 10:30:51 +0900

    6.3 アニメーションできるようにする

--- 初級Java2.5/src/DisplayFaceObject.java
+++ 初級Java2.5/src/DisplayFaceObject.java
@@ -1,10 +1,23 @@
 
-public class DisplayFaceObject extends MyFrame{
+public class DisplayFaceObject extends MyFrame {
 	public void run() {
-		Face face1 = new Face(50,50);
-		face1.draw(this);
 		
-		Face face2 = new Face(200,100);
-		face2.draw(this);
+		Face face1 = new Face(50, 50);
+
+		Face face2 = new Face(200, 100);
+
+		for (int i = 0; i < 30; i++) {
+			
+			clear();
+			face1.draw(this);
+			face2.draw(this);
+
+			face1.x += 10;
+			face1.y += 5;
+			face2.x -= 10;
+			face2.y -= 5;
+			
+			sleep(0.1);
+		}
 	}
 }
