f73d5d08bc8fd4ce5a147ee32b1912332bf03f00
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2024-05-28 11:42:07 +0900

    7.1 前章の復習

--- 初級Java2.5/src/DisplayFaceObject.java
+++ 初級Java2.5/src/DisplayFaceObject.java
@@ -5,15 +5,19 @@ public class DisplayFaceObject extends MyFrame {
 		Face face1 = new Face(50, 50, 10, 5);
 
 		Face face2 = new Face(200, 100, -10, -5);
+		
+		Face face3 = new Face(0, 100, 5, 0);
 
 		for (int i = 0; i < 30; i++) {
 
 			clear();
 			face1.draw(this);
 			face2.draw(this);
+			face3.draw(this);
 
 			face1.move();
 			face2.move();
+			face3.move();
 
 			sleep(0.1);
 		}
