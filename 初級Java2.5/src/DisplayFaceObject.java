c491d5c43c5fae84c284616fb952317dff302003
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2024-05-28 10:11:10 +0900

    確認問題6.2

--- 初級Java2.5/src/DisplayFaceObject.java
+++ 初級Java2.5/src/DisplayFaceObject.java
@@ -1,10 +1,13 @@
 
 public class DisplayFaceObject extends MyFrame{
 	public void run() {
-		Face face1 = new Face(50,50);
+		Face face1 = new Face(25,50);
 		face1.draw(this);
 		
-		Face face2 = new Face(200,100);
-		face2.draw(this);
+		Face face2 = new Face(125,150);
+		face2.draw(this);		
+		
+		Face face3 = new Face(225,50);
+		face3.draw(this);
 	}
 }
