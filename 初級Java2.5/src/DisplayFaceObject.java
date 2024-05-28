fe97b708baaf037310c95a85b9de39aade59a6a5
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2024-05-28 12:06:39 +0900

    7.3 要素を増やしてみましょう

--- 初級Java2.5/src/DisplayFaceObject.java
+++ 初級Java2.5/src/DisplayFaceObject.java
@@ -2,16 +2,18 @@
 public class DisplayFaceObject extends MyFrame {
 	public void run() {
 
-		Face[] faces = new Face[3];
+		Face[] faces = new Face[5];
 
 		faces[0] = new Face(50, 50, 10, 5);
 		faces[1] = new Face(200, 100, -10, -5);
 		faces[2] = new Face(0, 100, 5, 0);
+		faces[3] = new Face(200, 100, -5, 0);
+		faces[4] = new Face(100, 0, 0, 5);
 
 		for (int i = 0; i < 30; i++) {
 
 			clear();
-			for (int j = 0; j < 3; j++) {
+			for (int j = 0; j < faces.length; j++) {
 				faces[j].draw(this);
 				faces[j].move();
 			}
