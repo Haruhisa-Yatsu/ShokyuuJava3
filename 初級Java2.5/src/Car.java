56a95034be568069a7dc49c77fb84589feb9d73b
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2024-06-11 09:50:21 +0900

    9.2 キーボードに反応して動くようにする

--- 初級Java2.5/src/Car.java
+++ 初級Java2.5/src/Car.java
@@ -1,5 +1,7 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Car extends Vehicle {
+public class Car extends Vehicle implements KeyListener {
 
 	public Car(int x, int y, int vx, int vy) {
 		super(x, y, vx, vy);
@@ -10,6 +12,37 @@ public class Car extends Vehicle {
 		frame.fillRect(x, y + 20, 80, 20);
 		frame.fillOval(x + 10, y + 40, 20, 20);
 		frame.fillOval(x + 50, y + 40, 20, 20);
+	}
+
+	// キーから文字が入力されたとき
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	// キーが押されたとき
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			vx = -5;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			vx = 5;
+		}
+	}
+
+	// キーが離されたとき
+	@Override
+	public void keyReleased(KeyEvent e) {
+
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			vx = 0;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			vx = 0;
+		}
+
 	}
 
 }
--- 初級Java2.5/src/PlayerAnimation.java
+++ 初級Java2.5/src/PlayerAnimation.java
@@ -3,6 +3,7 @@ public class PlayerAnimation extends MyFrame{
 	public void run() {
 		Car player =new Car(100,100,0,0); 
 		
+		addKeyListener(player);
 		while(true) {
 			clear();
 			player.draw(this);
